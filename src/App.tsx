c2fdbe6d6d340c5ad471449e48f2c3fee451d795
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-18 09:57:17 +0100

    Styling the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,47 @@
- function App() {
-  return <div></div>;
+function App() {
+  return (
+    <div>
+      <BookList />
+    </div>
+  );
 }
 
-
 export default App;
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+function Book() {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+function Image() {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg"
+      alt="The Let them theory"
+    />
+  );
+}
+
+function Title() {
+  return <h1>Title</h1>;
+}
+
+function Author() {
+  return <h2>Mostafa</h2>;
+}

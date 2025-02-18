32adaeeeb3e7a26d4f31c040db5e2f17a1c855ca
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-18 19:13:32 +0100

    Styling the app

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,80 @@ function App() {
 
 export default App;
 
+const books = [
+  {
+    title: "The Let Them Theory",
+    author: "Mel Robbins",
+    image: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title: "Lorne: The Man Who Invented Saturday Night Live",
+    author: "Susan Marison",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81fIKUQ6lyL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    title: "Iron Flame",
+    author: "Rebecca Yarros",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+  {
+    title: "House of Flame and Shadow",
+    author: "Sarah J. Maas",
+    image:
+      "https://m.media-amazon.com/images/I/91n7p-j5aqL._AC_UF1000,1000_QL80_.jpg",
+    id: 4,
+  },
+
+  {
+    title: "The Heaven & Earth Grocery Store",
+    author: "James McBride",
+    image:
+      "https://m.media-amazon.com/images/I/81tdvyI0MeL._AC_UF1000,1000_QL80_.jpg",
+    id: 5,
+  },
+
+  {
+    title: "The Seven Husbands of Evelyn Hugo",
+    author: "Taylor Jenkins Reid",
+    image:
+      "https://m.media-amazon.com/images/I/71ZvnK+4JiL._AC_UF1000,1000_QL80_.jpg",
+    id: 6,
+  },
+  {
+    title: "Lessons in Chemistry",
+    author: "Bonnie Garmus",
+    image:
+      "https://m.media-amazon.com/images/I/71yNgTMEcpL._AC_UF1000,1000_QL80_.jpg",
+    id: 7,
+  },
+];
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => (
+        <Book {...book} key={book.id} />
+      ))}
     </section>
   );
 }
 
-function Book() {
+type BookProps = {
+  title: string;
+  author: string;
+  image: string;
+};
+
+function Book({ author, image, title }: BookProps) {
   return (
     <article className="book">
-      <img src="./images/book-1.jpg" alt="The Let them theory" />
-      <h1>Title</h1>
-      <h2>Mostafa</h2>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <h2>{author}</h2>
     </article>
   );
 }
